5a57be7bbf53abccd96e1d67085222572b9d12a3
Dan Collins <32271799+dacogemini@users.noreply.github.com>
2018-02-12 14:52:08 -0600

    Add files via upload

--- liri.js
+++ liri.js
@@ -4,151 +4,33 @@
 // * movie-this
 // * do-what-it-says
 
-//these add other programs to this one
-var dataKeys = require("./keys.js");
-var fs = require('fs'); //file system
-var twitter = require('twitter');
-var spotify = require('spotify');
+require('dotenv').config()
+// var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
 var request = require('request');
+// var omdb = require('omdb');
+var keys = require('./key.js')
 
 
-var writeToLog = function(data) {
-  fs.appendFile("log.txt", '\r\n\r\n');
+if (process.argv[2] === "my-tweets") {
 
-  fs.appendFile("log.txt", JSON.stringify(data), function(err) {
-    if (err) {
-      return console.log(err);
-    }
+    var client = new Twitter(keys.twitter);
 
-    console.log("log.txt was updated!");
-  });
-}
+    var params = {screen_name: 'dacogemini'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      if (!error) {
 
-//Creates a function for finding artist name from spotify
-var getArtistNames = function(artist) {
-  return artist.name;
-};
+          for (var i = 0; i < tweets.length; i++) {
 
-//Function for finding songs on Spotify
-var getMeSpotify = function(songName) {
-  //If it doesn't find a song, find Blink 182's What's my age again
-  if (songName === undefined) {
-    songName = 'What\'s my age again';
-  };
+              var twitterResults =
+                  "@" + tweets[i].user.screen_name + ": " +
+                  tweets[i].text + "\r\n" +
+                  tweets[i].created_at + "\r\n" +
+                  "-------------------------------------------------------------" + "\r\n";
+                  console.log(twitterResults);
 
-  spotify.search({ type: 'track', query: songName }, function(err, data) {
-    if (err) {
-      console.log('Error occurred: ' + err);
-      return;
-    }
+           };
 
-    var songs = data.tracks.items;
-    var data = []; //empty array to hold data
-
-    for (var i = 0; i < songs.length; i++) {
-      data.push({
-        'artist(s)': songs[i].artists.map(getArtistNames),
-        'song name: ': songs[i].name,
-        'preview song: ': songs[i].preview_url,
-        'album: ': songs[i].album.name,
-      });
-    }
-    console.log(data);
-    writeToLog(data);
-  });
-};
-
-
-var getTweets = function() {
-  var client = new twitter(dataKeys.twitterKeys);
-
-  var params = { screen_name: 'ednas', count: 10 };
-
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-
-    if (!error) {
-      var data = []; //empty array to hold data
-      for (var i = 0; i < tweets.length; i++) {
-        data.push({
-            'created at: ' : tweets[i].created_at,
-            'Tweets: ' : tweets[i].text,
-        });
       }
-      console.log(data);
-      writeToLog(data);
-    }
-  });
-};
-
-var getMeMovie = function(movieName) {
-
-  if (movieName === undefined) {
-    movieName = 'Mr Nobody';
-  }
-
-  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&r=json";
-
-  request(urlHit, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var data = [];
-      var jsonData = JSON.parse(body);
-
-      data.push({
-      'Title: ' : jsonData.Title,
-      'Year: ' : jsonData.Year,
-      'Rated: ' : jsonData.Rated,
-      'IMDB Rating: ' : jsonData.imdbRating,
-      'Country: ' : jsonData.Country,
-      'Language: ' : jsonData.Language,
-      'Plot: ' : jsonData.Plot,
-      'Actors: ' : jsonData.Actors,
-      'Rotten Tomatoes Rating: ' : jsonData.tomatoRating,
-      'Rotton Tomatoes URL: ' : jsonData.tomatoURL,
-  });
-      console.log(data);
-      writeToLog(data);
-}
-  });
-
-}
-
-var doWhatItSays = function() {
-  fs.readFile("random.txt", "utf8", function(error, data) {
-    console.log(data);
-    writeToLog(data);
-    var dataArr = data.split(',')
-
-    if (dataArr.length == 2) {
-      pick(dataArr[0], dataArr[1]);
-    } else if (dataArr.length == 1) {
-      pick(dataArr[0]);
-    }
-
-  });
-}
-
-var pick = function(caseData, functionData) {
-  switch (caseData) {
-    case 'my-tweets':
-      getTweets();
-      break;
-    case 'spotify-this-song':
-      getMeSpotify(functionData);
-      break;
-    case 'movie-this':
-      getMeMovie(functionData);
-      break;
-    case 'do-what-it-says':
-      doWhatItSays();
-      break;
-    default:
-      console.log('LIRI doesn\'t know that');
-  }
-}
-
-//run this on load of js file
-var runThis = function(argOne, argTwo) {
-  pick(argOne, argTwo);
-};
-
-runThis(process.argv[2], process.argv[3]);
+     });
+   }
